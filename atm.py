576c49b986c79c801fb6de22951190caed8eecfc
Zakiya435 <63589526+Zakiya435@users.noreply.github.com>
2021-09-13 16:59:09 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,14 @@
+class atm:
+    def __init__(self,cardNo,pin):
+        self.cardNo=cardNo
+        self.pin=pin
+
+    def CashWithdrawl(self):
+        print("You have withdrawed cash")
+
+    def BankInquiry(self):
+        print("Bank Details")
+
+    def Deposit(self):
+        print("You have deposited amount")
+        
