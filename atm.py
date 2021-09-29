388d282baee098e1bdaf47e9e2b313ceec9490e8
Zakiya435 <63589526+Zakiya435@users.noreply.github.com>
2021-09-29 14:54:52 +0530

    Update atm.py

--- atm.py
+++ atm.py
@@ -1,14 +1,29 @@
 class atm:
-    def __init__(self,cardNo,pin):
+    def __init__(self,cardNo,pin, userId,bal):
         self.cardNo=cardNo
         self.pin=pin
+        self.userId = userId
+        self.bal = bal
 
     def CashWithdrawl(self):
-        print("You have withdrawed cash")
+        amount = int(input("Enter the amount to withdraw"))
+        self.bal = self.bal-amount
+        print(self.bal)
 
     def BankInquiry(self):
-        print("Bank Details")
+        print("This is your balance: "+self.bal)
 
     def Deposit(self):
-        print("You have deposited amount")
-        
+       amount = int(input("Enter the amount to deposit"))
+       self.bal = self.bal+amount
+       print(self.bal)
+
+one = atm(123,1000,786,110000)
+choice = int(input("For Withdraw press 1, for checking press 2, for deposit press 3: "))
+
+if choice == '1':
+    one.CashWithdrawl()
+elif choice == '2':
+    one.BankInquiry()
+elif choice == '3':
+    one.Deposit()
